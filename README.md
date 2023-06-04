bff54f9b9e0c26e73483553b2d6002e88f389209
jjk <jjk@beyondapp.com>
2023-06-05 01:15:48 +0900

    matplotlib 그림 그리기 추가

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # elevation_list_googlemaps
-구글 맵스를 이용해서 정해진 두 좌표의 고도 리스트를  뽑아내는 파이썬 프로그램
+구글 맵스를 이용해서 정해진 두 좌표의 고도 리스트를 뽑아내서 matplotlib로 그리는 파이썬 프로그램
 
 ```
-pip3 install googlemaps polyline
+pip3 install googlemaps polyline matplotlib streamlit
+```
+----
+
+
+```
+python3 app.py
 ```
 
+> 샌프란시코에서 엘에이
 
+![샌프란시코에서 엘에이](SF_to_LA.png)
+
+
+> 오리교에서 불곡산
+![오리교_불곡산정상.png](오리교_불곡산정상.png)
--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import polyline
 api_key = 'AIzaSyAQ5aElKgEv8I0Bz1cVG7lVvmMhalRr-Vg'
 
 # 시작점과 도착점의 좌표를 입력하세요.
-start = (37.7749, -122.4194)  # 예시 좌표 (샌프란시스코)
-end = (34.0522, -118.2437)  # 예시 좌표 (로스앤젤레스)
+# start = (37.7749, -122.4194)  # 예시 좌표 (샌프란시스코)
+# end = (34.0522, -118.2437)  # 예시 좌표 (로스앤젤레스)
+
+start = (37.338407, 127.112792)  # 예시 좌표 (오리교)
+end = (37.351574, 127.133927)  # 예시 좌표 (불곡산정상)
 
 # Google Maps 클라이언트를 생성합니다.
 gmaps = googlemaps.Client(key=api_key)
@@ -19,3 +22,20 @@ elevation_list = [point['elevation'] for point in elevation_data]
 
 # 결과를 출력합니다.
 print(elevation_list)
+
+
+import matplotlib.pyplot as plt
+
+# elevation_list 데이터를 가지고 막대 그래프를 그립니다.
+plt.bar(range(len(elevation_list)), elevation_list)
+
+# 그래프의 타이틀과 축 제목을 설정합니다.
+plt.title('Elevation Profile')
+plt.xlabel('Distance')
+plt.ylabel('Elevation')
+
+# x축 눈금을 설정합니다.
+plt.xticks([])  # 현재는 눈금을 비활성화했습니다. 필요에 따라 설정하십시오.
+
+# 그래프를 출력합니다.
+plt.show()
